89ee50d1df78355208bbc73043c82cfb13f3a1b2
Kaiyrgaliyev <damir240507@gmail.com>
2025-03-29 21:07:41 +0500

    Update and rename views.py to admin.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from models11 import Product
-
-
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
